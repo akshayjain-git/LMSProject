c2cd7924a6ba749b964c7235751928c70968b7cd
Akshay Jain <akkujain029z@gmail.com>
2026-05-09 12:50:16 +0530

    Update authcontroller.js

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -26,8 +26,8 @@ export const signup = async (req,res) => {
         let token = await GenToken(user._Id)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite: "strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000
         })
         return res.status(201).json(user)
@@ -50,8 +50,8 @@ export const login = async (req,res) => {
         let token = await GenToken(user._Id)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite: "strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7*24*60*60*1000
         })
         return res.status(200).json(user)
